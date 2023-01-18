190b7ed4c327c48506d452b7a6da537e1f4338d5
bskydr <62293824+bskydr@users.noreply.github.com>
2023-01-18 19:49:21 -0400

    files added

---
+++ index-es.php
@@ -0,0 +1,318 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="http://kydpro.com">
+
+    <title>kydpro.com - Soluciones TI en el mundo entero</title>
+
+    <!-- Bootstrap Core CSS - Uses Bootswatch Flatly Theme: http://bootswatch.com/flatly/ -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/freelancer.css" rel="stylesheet">
+	
+	<!-- Custom CSS -->
+    <link href="css/changed.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<?php include_once("analyticstracking.php") ?>
+</head>
+
+<body id="page-top" class="index">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#page-top" style="padding:0; margin:0; margin-top:-15px;"><img src="images/kydpro.com.png" alt="KimTech.net" class="img-responsive"/></a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+					 <li class="page-scroll">
+                        <a href="#services">Servicios</a>
+                    </li>
+                    <!--<li class="page-scroll">
+                        <a href="#portfolio">Portfolio</a>
+                    </li>-->
+                    <li class="page-scroll">
+                        <a href="#about">Acerca de nosotros</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="#contact">Contacto</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
+    <!-- Header -->
+    <header>
+        <div class="container">
+            <div class="row">
+			
+                <div class="col-lg-12">
+				<p style="text-align:right;"><a style="color:yellow;" href="index.php" alt="Change language to English" title="Change language to English"><i class="fa fa-language"></i> English</a></p>
+                    <img class="img-responsive" src="img/logo_perfil.png" alt="">
+                    <div class="intro-text">
+                        <span class="name">Soluciones TI</span>
+                        <hr class="star-light">
+                        <span class="skills">Desarrollo web - Diseño gráfico - Servidores y alojamiento web - Soporte TI y mucho más.</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+	 <!-- Services -->
+	<section id="services">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Servicios</h2>
+                    <hr class="star-primary">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-4 portfolio-item">
+						<div class="caption">
+                            <div class="caption-content">
+                               Administración de Medios Sociales
+                            </div>
+                        </div>
+                        <img src="images/social_media_management.png" class="img-responsive" alt="Social media management">
+                    <!--</a>-->
+                </div>
+                <div class="col-sm-4 portfolio-item">
+						<div class="caption">
+                            <div class="caption-content">
+                                Optimización de Motores de Búsqueda (SEO)
+                            </div>
+                        </div>
+                        <img src="images/SEO.png" class="img-responsive" alt="Search engine optimization (SEO)">
+                   
+                </div>
+                <div class="col-sm-4 portfolio-item">
+                   <div class="caption">
+                            <div class="caption-content">
+                               Servidores y Alojamiento web
+                            </div>
+                        </div>
+                        <img src="images/servers_and_webhosting.png" class="img-responsive" alt="Servers and web hosting">
+                  
+                </div>
+                <div class="col-sm-4 portfolio-item">
+                   <div class="caption">
+                            <div class="caption-content">
+                               Diseño y mantenimiento web
+                            </div>
+                        </div>
+                        <img src="images/website-design-developer-maintenance.png" class="img-responsive" alt="Website design and maintenance">
+                   
+                </div>
+                <div class="col-sm-4 portfolio-item">
+                   <div class="caption">
+                            <div class="caption-content">
+                               Soporte TI y acesoría
+                            </div>
+                        </div>
+                        <img src="images/it-support-advice.png" class="img-responsive" alt="IT support and advice">
+                   
+                </div>
+                <div class="col-sm-4 portfolio-item">
+                    <div class="caption">
+                            <div class="caption-content">
+                               Programas y soluciones
+                            </div>
+                        </div>
+                        <img src="images/software-and-solutions.png" class="img-responsive" alt="Software and solutions">
+                   
+                </div>
+            </div>
+        </div>
+    </section>
+	
+
+    <!-- About Section -->
+    <section class="success" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Acerca de Nosotros</h2>
+                    <hr class="star-light">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-lg-offset-2">
+                    <p>kydpro.com es una empresa con mas de <?php echo date('Y')-2006;?> años en el mercado ofreciendo soluciones TI a empresas y publico en general según sus necesidades. Ofrecemos un amplio rango de productos y servicios relacionados con la computación y la tecnología.</p>
+                </div>
+                <div class="col-lg-4">
+                    <p>Si usted es una empresa o persona particular buscando construir o que le administren su website, medios sociales, comprar un dominio, servidor/alojamiento, administrar su estructura TI o acesoría sobre TI. Somos la empresa profesional que usted esta buscando!</p>
+                </div>
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <a href="#" class="btn btn-lg btn-outline">
+                        <i class="fa fa-question-circle"></i> Más información
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Contact Section -->
+    <section id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Contáctenos</h2>
+                    <hr class="star-primary">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2">
+                   
+                    <form name="sentMessage" id="contactForm" novalidate>
+                        <div class="row control-group">
+                            <div class="form-group col-xs-12 floating-label-form-group controls">
+                                <label>Nombre</label>
+                                <input type="text" class="form-control" placeholder="Nombre" id="name" required data-validation-required-message="Por favor, entre su nombre.">
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <div class="row control-group">
+                            <div class="form-group col-xs-12 floating-label-form-group controls">
+                                <label>Correo electrónico</label>
+                                <input type="email" class="form-control" placeholder="Correo" id="email" required data-validation-required-message="Por favor, entre su correo electrónico.">
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <div class="row control-group">
+                            <div class="form-group col-xs-12 floating-label-form-group controls">
+                                <label>Teléfono</label>
+                                <input type="tel" class="form-control" placeholder="Teléfono" id="phone" required data-validation-required-message="Por favor, entre su número de teléfono.">
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <div class="row control-group">
+                            <div class="form-group col-xs-12 floating-label-form-group controls">
+                                <label>Mensaje</label>
+                                <textarea rows="5" class="form-control" placeholder="Mensaje" id="message" required data-validation-required-message="Por favor, entre un mensaje."></textarea>
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <br>
+                        <div id="success"></div>
+                        <div class="row">
+                            <div class="form-group col-xs-12">
+                                <button type="submit" class="btn btn-success btn-lg">Enviar</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Footer -->
+    <footer class="text-center">
+        <div class="footer-above">
+            <div class="container">
+                <div class="row">
+                    <div class="footer-col col-md-4">
+                        
+                    </div>
+                    <div class="footer-col col-md-4">
+                        <h3>Medios Sociales</h3>
+                        <ul class="list-inline">
+                            <li>
+                                <a target="_blank" href="https://www.facebook.com/kydpro" class="btn-social btn-outline"><i class="fa fa-fw fa-facebook"></i></a>
+                            </li>
+                            <li>
+                                <a target="_blank" href="https://plus.google.com/u/3/108039825157177437228" class="btn-social btn-outline"><i class="fa fa-fw fa-google-plus"></i></a>
+                            </li>
+                            <li>
+                                <a target="_blank" href="https://twitter.com/KydproC" class="btn-social btn-outline"><i class="fa fa-fw fa-twitter"></i></a>
+                            </li>
+                            <li>
+                                <a target="_blank" href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-linkedin"></i></a>
+                            </li>
+                            <li>
+                                <a target="_blank" href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-dribbble"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="footer-col col-md-4">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-below">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        Derecho de autor &copy; www.kydpro.com <?php echo date('Y');?> - Desarrollado por: <a href="http://www.kydpro.com/">www.kydpro.com</a> 
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
+    <div class="scroll-top page-scroll hidden-sm hidden-xs hidden-lg hidden-md">
+        <a class="btn btn-primary" href="#page-top">
+            <i class="fa fa-chevron-up"></i>
+        </a>
+    </div>
+
+
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="js/classie.js"></script>
+    <script src="js/cbpAnimatedHeader.js"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="js/freelancer.js"></script>
+
+</body>
+
+</html>
